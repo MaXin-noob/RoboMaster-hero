3452385e22a9a45214ba2b2f167ecb723253dd9c
15128381962 <1501655237@qq.com>
2020-10-29 00:52:35 +0800

    Update shootTask.c
    修改开启摩擦轮时拨盘旋转的bug

--- hero_new/Src/shootTask.c
+++ hero_new/Src/shootTask.c
@@ -56,25 +56,27 @@ static int pluck_flag = 0;
 static int scanning_flag = 0;
 static int jam_time_1 = 0;
 static int jam_time_2 = 0;
+static int pluck_one_flag = 0;
+static int B_time = 0;
 /**
  * @description: shoot control
  * @param[in]:void const *argument
  * @return: void
  */
 void shootHandler(void const *argument) {
-  uint32_t period = osKernelSysTick();
-  while (1) {
-    frictiongearControl();  //摩擦轮控制
-    pluckControl();         // 拨盘控制
-                            //  scanningShoot();        // 卡弹扫描
+    uint32_t period = osKernelSysTick();
+    while (1) {
+        frictiongearControl();  //摩擦轮控制
+        pluckControl();         // 拨盘控制
+        //  scanningShoot();        // 卡弹扫描
 #if FORBID_FIRCTIONGEAR == 0
-    sendCanData(&hcan1, RM3510_RM3508_ID_Send_0x200,
-                shoot.shoot_pid_out.frictiongear_left_OUT,
-                shoot.shoot_pid_out.frictiongear_right_OUT, 0,
-                shoot.shoot_pid_out.pluck_OUT);
+        sendCanData(&hcan1, RM3510_RM3508_ID_Send_0x200,
+                    shoot.shoot_pid_out.frictiongear_left_OUT,
+                    shoot.shoot_pid_out.frictiongear_right_OUT, 0,
+                    shoot.shoot_pid_out.pluck_OUT);
 #endif /*end FORBID_FIRCTIONGEAR*/
-    osDelayUntil(&period, 6);
-  }
+        osDelayUntil(&period, 6);
+    }
 }
 /**
  * @description: frictiongear control
@@ -82,35 +84,49 @@ void shootHandler(void const *argument) {
  * @return: void
  */
 static void frictiongearControl(void) {
-  if (gimbal.gimbal_mode == remote) {
-    if (getDbusState(&rcDevice, RC_S2_MID2UP) == Hero_OK) {
-      if (shoot.frictiongear_flag == false) {
-        shoot.frictiongear_flag = true;
-      } else {
-        shoot.frictiongear_flag = false;
-      }
+    if (gimbal.gimbal_mode == remote) {
+        if (getDbusState(&rcDevice, RC_S2_MID2UP) == Hero_OK) {
+            if (shoot.frictiongear_flag == false) {
+                shoot.frictiongear_flag = true;
+            } else {
+                shoot.frictiongear_flag = false;
+            }
+        }
     }
-  }
-  if (gimbal.gimbal_mode == keyboard) {
-    if (rcDevice.rc_info.mouse.l == true) {
-      shoot.frictiongear_flag = true;
+    if (gimbal.gimbal_mode == keyboard) {
+        if (rcDevice.rc_info.mouse.l == true) {
+            shoot.frictiongear_flag = true;
+
+        }
+        if (rcDevice.rc_info.mouse.l - rcDevice.last_rc_info.mouse.l == -1) {
+            pluck_one_flag++;
+        }
+        if (rcDevice.rc_info.kb.bit.B == true) {
+            B_time ++;
+            if (B_time >= 270) {
+                shoot.frictiongear_flag = false;
+                pluck_one_flag = 0;
+            }
+        } else {
+            B_time = 0;
+        }
     }
-  }
-  if (shoot.frictiongear_flag == true) {  //开摩擦轮
-    shoot.shoot_pid_out.frictiongear_left_OUT = pid_calculate(
-        shoot.shoot_pid.frictiongear_left_PID,
-        shoot.shoot_realy_speed.frictiongear_left_speed, -frictiongear_Speed);
-    shoot.shoot_pid_out.frictiongear_right_OUT = pid_calculate(
-        shoot.shoot_pid.frictiongear_right_PID,
-        shoot.shoot_realy_speed.frictiongear_right_speed, frictiongear_Speed);
-  } else {  //关摩擦轮
-    shoot.shoot_pid_out.frictiongear_left_OUT =
-        pid_calculate(shoot.shoot_pid.frictiongear_left_PID,
-                      shoot.shoot_realy_speed.frictiongear_left_speed, 0);
-    shoot.shoot_pid_out.frictiongear_right_OUT =
-        pid_calculate(shoot.shoot_pid.frictiongear_right_PID,
-                      shoot.shoot_realy_speed.frictiongear_right_speed, 0);
-  }
+
+    if (shoot.frictiongear_flag == true) {  //开摩擦轮
+        shoot.shoot_pid_out.frictiongear_left_OUT = pid_calculate(
+                    shoot.shoot_pid.frictiongear_left_PID,
+                    shoot.shoot_realy_speed.frictiongear_left_speed, -frictiongear_Speed);
+        shoot.shoot_pid_out.frictiongear_right_OUT = pid_calculate(
+                    shoot.shoot_pid.frictiongear_right_PID,
+                    shoot.shoot_realy_speed.frictiongear_right_speed, frictiongear_Speed);
+    } else {  //关摩擦轮
+        shoot.shoot_pid_out.frictiongear_left_OUT =
+            pid_calculate(shoot.shoot_pid.frictiongear_left_PID,
+                          shoot.shoot_realy_speed.frictiongear_left_speed, 0);
+        shoot.shoot_pid_out.frictiongear_right_OUT =
+            pid_calculate(shoot.shoot_pid.frictiongear_right_PID,
+                          shoot.shoot_realy_speed.frictiongear_right_speed, 0);
+    }
 }
 /**
  * @description: pluck control
@@ -118,35 +134,40 @@ static void frictiongearControl(void) {
  * @return: void
  */
 static void pluckControl(void) {
-  float position_out;
-  if (shoot.frictiongear_flag == true) {
-    if (gimbal.gimbal_mode == remote) {
-      if ((getDbusState(&rcDevice, RC_S2_MID2DOWN) == Hero_OK) &&
-          (pluck_flag == 0) &&
-          JudgementData.power_heat_data_t.shooter_heat1 <= 100) {
-        shoot.pluck_angle += pluck_Rotator;
-        pluck_flag = 1;
-      }
-      if (getDbusState(&rcDevice, RC_S2_DOWN2MID) == Hero_OK) {
-        pluck_flag = 0;
-      }
+    float position_out;
+    if (gimbal.gimbal_mode == remote) {  //遥控模式
+        if (shoot.frictiongear_flag == true) {
+            if ((getDbusState(&rcDevice, RC_S2_MID2DOWN) == Hero_OK) &&
+                    (pluck_flag == 0) &&
+                    JudgementData.power_heat_data_t.shooter_heat1 <= 100) {
+                shoot.pluck_angle += pluck_Rotator;
+                pluck_flag = 1;
+            }
+            if (getDbusState(&rcDevice, RC_S2_DOWN2MID) == Hero_OK) {
+                pluck_flag = 0;
+            }
+          
+        }
     } else if (gimbal.gimbal_mode == keyboard) {
-      if ((rcDevice.rc_info.mouse.l == true) && (pluck_flag == 0) &&
-          JudgementData.power_heat_data_t.shooter_heat1 <= 100) {
-        shoot.pluck_angle += pluck_Rotator;
-        pluck_flag = 1;
-      }
-      if (rcDevice.rc_info.mouse.l == false) {
-        pluck_flag = 0;
-      }
+        if ((shoot.frictiongear_flag == true) && (pluck_one_flag >= 2)) {
+            if ((rcDevice.rc_info.mouse.l == true) && (pluck_flag == 0) &&
+                    JudgementData.power_heat_data_t.shooter_heat1 <= 100) {
+                shoot.pluck_angle += pluck_Rotator;
+                pluck_flag = 1;
+            }
+            if (rcDevice.rc_info.mouse.l == false) {
+                pluck_flag = 0;
+            }
+        }
+
     }
-  }
-  position_out = pid_calculate(shoot.shoot_pid.pluck_positio_PID,
-                               shoot.shoot_encoder.pluck_encoder->ecd_angle,
-                               shoot.pluck_angle);
-  shoot.shoot_pid_out.pluck_OUT = pid_calculate(
-      shoot.shoot_pid.pluck_speed_PID,
-      shoot.shoot_encoder.pluck_encoder->filter_rate, position_out);
+
+    position_out = pid_calculate(shoot.shoot_pid.pluck_positio_PID,
+                                 shoot.shoot_encoder.pluck_encoder->ecd_angle,
+                                 shoot.pluck_angle);
+    shoot.shoot_pid_out.pluck_OUT = pid_calculate(
+                                        shoot.shoot_pid.pluck_speed_PID,
+                                        shoot.shoot_encoder.pluck_encoder->filter_rate, position_out);
 }
 /**
  * @description: jamming scanning
@@ -154,29 +175,29 @@ static void pluckControl(void) {
  * @return: void
  */
 static void scanningShoot(void) {
-  if ((shoot.pluck_angle - shoot.shoot_encoder.pluck_encoder->ecd_angle > 60) &&
-      (scanning_flag = false)) {
-    if (shoot.shoot_encoder.pluck_encoder->filter_rate <= 50) {
-      jam_time_1 += 1;
-      if (jam_time_1 == 80) {
-        scanning_flag = true;
-        shoot.pluck_angle = shoot.shoot_encoder.pluck_encoder->ecd_angle - 200;
-      }
+    if ((shoot.pluck_angle - shoot.shoot_encoder.pluck_encoder->ecd_angle > 60) &&
+            (scanning_flag = false)) {
+        if (shoot.shoot_encoder.pluck_encoder->filter_rate <= 50) {
+            jam_time_1 += 1;
+            if (jam_time_1 == 80) {
+                scanning_flag = true;
+                shoot.pluck_angle = shoot.shoot_encoder.pluck_encoder->ecd_angle - 200;
+            }
+        } else {
+            jam_time_1 = 0;
+        }
     } else {
-      jam_time_1 = 0;
+        jam_time_1 = 0;
     }
-  } else {
-    jam_time_1 = 0;
-  }
-  if (scanning_flag == true) {
-    if (jam_time_2 == 100 ||
-        (shoot.pluck_angle - shoot.shoot_encoder.pluck_encoder->ecd_angle >
-         -10)) {
-      jam_time_2 = 0;
-      scanning_flag = true;
-      shoot.pluck_angle += pluck_Rotator;
-    } else {
-      jam_time_2 += 1;
+    if (scanning_flag == true) {
+        if (jam_time_2 == 100 ||
+                (shoot.pluck_angle - shoot.shoot_encoder.pluck_encoder->ecd_angle >
+                 -10)) {
+            jam_time_2 = 0;
+            scanning_flag = true;
+            shoot.pluck_angle += pluck_Rotator;
+        } else {
+            jam_time_2 += 1;
+        }
     }
-  }
 }
